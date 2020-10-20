9c2bb9ccd25b14bd9e27e421c8ef0f8040db7345
Joe Gibson <joseph.gibson@nasa.gov>
2020-10-20 14:45:25 -0400

    Tweaks to animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -111,9 +111,9 @@ $(function () {
 	 */
 
 	$(".rotate").textrotator({
-		animation: "fade", 			// You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+		animation: "dissolve", 		// You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
 		separator: ",", 			// If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-		speed: 3800 				// How many milliseconds until the next word shows.
+		speed: 3000 				// How many milliseconds until the next word shows.
 	});
 
 
